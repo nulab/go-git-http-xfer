1be2b3662b811e25eb548c98f8e55a54fd42671d
Yuichi Watanabe <yuichi.watanabe.ja@gmail.com>
2018-11-15 11:36:37 +0900

    remove log from clean up function (#5)

--- githttpxfer/githttpxfer.go
+++ githttpxfer/githttpxfer.go
@@ -7,7 +7,6 @@ import (
 	"net/http"
 	"net/url"
 	"os"
-	"os/exec"
 	"regexp"
 	"strings"
 	"sync"
@@ -267,11 +266,7 @@ func (ghx *GitHTTPXfer) serviceRPC(ctx Context, rpc string) {
 	args := []string{rpc, "--stateless-rpc", "."}
 	cmd := ghx.Git.GitCommand(repoPath, args...)
 	cmd.Env = ctx.Env()
-	defer func(cmd *exec.Cmd) {
-		if err := cleanUpProcessGroup(cmd); err != nil {
-			ghx.logger.Error("clean up process group -> ", err.Error())
-		}
-	}(cmd)
+	defer cleanUpProcessGroup(cmd)
 
 	stdin, err := cmd.StdinPipe()
 	if err != nil {
@@ -289,7 +284,8 @@ func (ghx *GitHTTPXfer) serviceRPC(ctx Context, rpc string) {
 	}
 	defer stdout.Close()
 
-	if err = cmd.Start(); err != nil {
+	err = cmd.Start()
+	if err != nil {
 		ghx.logger.Error("failed to starts the specified command. ", err.Error())
 		RenderInternalServerError(res.Writer)
 		return
